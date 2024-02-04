e0b678fabdca416a4f19e208e12df9cf57cb3852
msoucek <soucek_milan@centrum.cz>
2024-02-04 20:38:03 +0100

    znovu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import cz.engeto.ja.guests.Guest;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Guest> guest = new ArrayList<>());
+        guest.add(new Guest("Adela", "Malikova", "13.3.1993"));
+        guest.add(new Guest("Jana", "Dvoracka", "5.5.1995"));
+
+        for (int i = 0; i < 8; i++){
+            guest.add(new Guest("" + i, "",));
+        }
+        for (Guest guest : guest) {
+            System.out.println(guest.getName()+guest.getSurname()+guest.getBirthdate());
+        }
     }
 }
